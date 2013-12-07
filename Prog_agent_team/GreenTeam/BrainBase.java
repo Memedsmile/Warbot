fed67be54f7c523329c819490105dd0ee6409651
Chartier Theo <theo.chartier@etud.univ-montp2.fr>
2013-12-07 09:40:16 +0100

    ajout modif GreenTeam

--- Prog_agent_team/GreenTeam/BrainBase.java
+++ Prog_agent_team/GreenTeam/BrainBase.java
@@ -7,24 +7,81 @@ import edu.turtlekit2.warbot.message.WarMessage;
 
 public class BrainBase extends WarBrain{
 	
+	int cpt =0;
+	int nb=0;
 
 	public BrainBase(){
 		
 	}
 
+	
+	//Retourne le nombre d'explorer encore vivant
+	public int getEffectifExplorer()
+	{
+		broadcastMessage("WarExplorer","Effectif",null);
+		List<WarMessage> liste = getMessage();
+		
+		int nb = 0;
+		for (WarMessage m : liste)
+		{  
+			if (m.getMessage() == "present")
+				nb++;
+		}
+		System.out.println(nb);
+		return nb;
+	}
+	
 	@Override
 	public String action() {
 		
+		cpt++;
 		if(!emptyBag()){
 			return "eat";
 		}
-
-		List<WarMessage> liste = getMessage();
+		nb = 0;
 		
+		
+	
+		
+		if (cpt == 3)
+		{	
+			cpt = 0;
+			this.broadcastMessage("WarExplorer", "Effectif",null);
+		}
+		List<WarMessage> liste = getMessage();
 		for(WarMessage m : liste){
+			
+			if (m.getMessage() == "present")
+				nb++;
 			
 			if (m.getMessage() == "where")
 				reply(m, "ici", null);
+		}
+		
+		//Nb est le nombre effectif
+		if (nb > 0)
+		{
+			//Distribution des taĉhes
+			broadcastMessage("WarExplorer","Metier",null);
+			
+			int totalDistance = 0;
+			
+			for (WarMessage m : liste)
+			{
+				if(m.getMessage() == "jenvoie_ma_position")
+					totalDistance += m.getDistance(); 
+			}
+			for (WarMessage m : liste)
+			{
+				if(m.getMessage() == "jenvoie_ma_position")
+				{
+					if(m.getDistance() < totalDistance)
+						reply(m,"tu seras ceuilleur",null);
+					else
+						reply(m,"tu seras espion",null);
+					
+				}
+			}
 		}
 		
 		if(getEnergy() > 12000){
--- Prog_agent_team/GreenTeam/BrainExplorer.java
+++ Prog_agent_team/GreenTeam/BrainExplorer.java
@@ -10,9 +10,26 @@ import edu.turtlekit2.warbot.waritems.WarFood;
 public class BrainExplorer extends WarBrain{
 	
 	int taille_sac = 3;
+	String mode;
 	
 	public BrainExplorer(){
 		taille_sac = 3;
+
+			mode = "ceuilleur";
+			
+	}
+	
+	
+	public Percept getEnemyBasePercept(List<Percept> listeP)
+	{
+		Percept EnemyBasePercept = null;
+		for(Percept p : listeP){
+			if(p.getType().equals("WarBase") && !p.getTeam().equals(getTeam())){
+				EnemyBasePercept = p;
+			}
+		}
+		return EnemyBasePercept;
+		
 	}
 	
 	public Percept getFriendlyBestBase(List<Percept> listeP){
@@ -48,63 +65,107 @@ public class BrainExplorer extends WarBrain{
 		return bestFood;
 		
 	}
-	@Override
-	public String action() {
-		
-		if (taille_sac == 0)
-		{
-			System.out.println("J'ai tout donner");
-			taille_sac = 3;
-		}
-		else if (taille_sac < 3)
-		{
-			taille_sac--;
-			return "give";
-		}
+	
+	String reflexes(String mode)
+	{
 		List<Percept> liste = getPercepts();
 		List<WarMessage> listeM = getMessage();
 		
-		if(liste.size()>0 && !fullBag()){
-			Percept bestFood = null;
+		//Repondre au message - a mettre en action
+		for (WarMessage m : listeM)
+		{
+			if (m.getMessage() == "Effectif")
+				reply(m,"present",null);
 			
-		   bestFood = getBestFood(liste); 
-		   
-			if(bestFood != null && bestFood.getDistance() < WarFood.MAX_DISTANCE_TAKE){
-				return "take";
-			//Si la nourriture est hors porté de prise 
-			}else if(bestFood != null && bestFood.getType().equals("WarFood")){
-				setHeading(bestFood.getAngle());
-			}else{
-				while(isBlocked()){
-					setRandomHeading();
-				}
+			if(m.getMessage() == "Metier")
+				reply(m,"jenvoie_ma_position",null);
+			
+			if(m.getMessage().startsWith("tu seras"))
+			{
+				String metier = m.getMessage().substring(9);
+				if (metier == "espion")
+					System.out.println("ok1");
+				else
+					System.out.println("ok2");
 			}
-		}else if(liste.size()>0 && fullBag()){
-			Percept bestBase = null;
+		}
 		
-			bestBase = this.getFriendlyBestBase(liste);
-			if(bestBase != null && bestBase.getDistance() < WarFood.MAX_DISTANCE_TAKE){
-				setAgentToGive(bestBase.getId());
+		if (mode == "ceuilleur")
+		{
+			if (taille_sac == 0)
+			{
+				System.out.println("J'ai tout donner");
+				taille_sac = 3;
+			}
+			else if (taille_sac < 3)
+			{
 				taille_sac--;
 				return "give";
-			}else if(bestBase != null && bestBase.getType().equals("WarFood")){
-				setHeading(bestBase.getAngle());
+			}
+			
+			if(liste.size()>0 && !fullBag()){
+				Percept bestFood = null;
+				
+			   bestFood = getBestFood(liste); 
+			   
+				if(bestFood != null && bestFood.getDistance() < WarFood.MAX_DISTANCE_TAKE){
+					return "take";
+				//Si la nourriture est hors porté de prise 
+				}else if(bestFood != null && bestFood.getType().equals("WarFood")){
+					setHeading(bestFood.getAngle());
+				}else{
+					while(isBlocked()){
+						setRandomHeading();
+					}
+				}
+			}else if(liste.size()>0 && fullBag()){
+				Percept bestBase = null;
+			
+				bestBase = this.getFriendlyBestBase(liste);
+				if(bestBase != null && bestBase.getDistance() < WarFood.MAX_DISTANCE_TAKE){
+					setAgentToGive(bestBase.getId());
+					taille_sac--;
+					return "give";
+				}else if(bestBase != null && bestBase.getType().equals("WarFood")){
+					setHeading(bestBase.getAngle());
+				}else{
+					while(isBlocked()){
+						setRandomHeading();
+					}
+				}
+			}
+			//Si le sac est plein et que la base ne nous a pas envoyé de message
+			else if(fullBag() && listeM.size()==0){
+				broadcastMessage("WarBase", "where", null);
+			}else if(fullBag() && listeM.size()>0){
+				setHeading(listeM.get(0).getAngle());
 			}else{
 				while(isBlocked()){
 					setRandomHeading();
 				}
 			}
+			return "move";
 		}
-		//Si le sac est plein et que la base ne nous a pas envoyé de message
-		else if(fullBag() && listeM.size()==0){
-			broadcastMessage("WarBase", "where", null);
-		}else if(fullBag() && listeM.size()>0){
-			setHeading(listeM.get(0).getAngle());
-		}else{
-			while(isBlocked()){
-				setRandomHeading();
+		else if (mode == "espion")
+		{
+			if (liste.size() > 0){
+				Percept basePercept=null;
+				basePercept = this.getEnemyBasePercept(liste);
+				if(basePercept != null){
+					broadcastMessage("WarBase", "BaseSpotted", null);
+				}
 			}
+		//	System.out.println("Je suis un espion");
 		}
-		return "move";
+		return null;
+	}
+	@Override
+	public String action() {
+		
+		String act;
+		act = reflexes(mode);
+		if (act != null)
+			return act;
+		return null;
 	}
 }
